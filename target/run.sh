b9d0bb9ceed7ec36db9b603d873d37afb6ab9184
Stacie Waleyko <staciewaleyko@gmail.com>
2018-11-12 12:41:45 -0500

    Remove target directory

--- target/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Parse inputs and add them to specific user arguments here
-USER_ARGS=""
-
-COMPILATION_CLASSPATH=`find -L . -maxdepth 1 -name "*.jar" -type f -exec printf :{} ';'`
-
-java $JAVA_OPTS $USER_ARGS -cp $COMPILATION_CLASSPATH io.gatling.app.Gatling -s MySimulation
